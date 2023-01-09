9a536d7455935b57fa22d4fefda025240423fda5
Andrzej Sędzik <39155518+andrzej-sedzik-dev@users.noreply.github.com>
2023-01-09 23:50:28 +0100

    8.2 try catch finally

---
+++ kodilla-exception/src/main/java/com/kodillaexception/main/ExceptionModuleRunner.java
@@ -0,0 +1,12 @@
+package com.kodillaexception.main;
+
+import com.kodillaexception.io.FileReader;
+
+public class ExceptionModuleRunner {
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
+
+}
